3e1e56c56523982e3aaf443a2119a3a6bf160c29
guhcalm <guhcalm@outlook.com>
2022-04-21 14:11:10 -0400

    if-statement

---
+++ 3-if-statement/3-if-statement/script.ps1
@@ -0,0 +1,59 @@
+# if statements hade personalized Operators
+
+# Operator	        Comparison
+#_______________________________
+# -eq	            equals
+# -ne	            not equals
+# -gt	            greater than
+# -ge	            greater than or equal
+# -lt	            less than
+# -le	            less than or equal
+# -like	            string matches wildcard pattern
+# -notlike	        string does not match wildcard pattern
+# -match	        string matches regex pattern
+# -notmatch	        string does not match regex pattern
+# -contains	        collection contains a vlaue
+# -notcontains	    collection does not contain a value
+# -in	            value is in a collection
+# -notin	        value is not in a collection
+# -is	            both objects are the same type
+# -isnot	        the objects are not the same type
+
+#Statement Syntax
+# if ( condition ) { statement of command } else { statement of command }
+
+[ int ] $eggs = 10
+if ( $eggs -lt 12 ) {
+    echo 'You have lass than dozen eggs'
+}
+
+[ int ] $x = 4
+if ( $x -gt 3 ) {
+    echo "$x is greater than or equal to 3"
+} elseif ( $x -eq 4 ) {
+    echo "$x is equals 4"
+} else {
+    echo "$x is lass than 3"
+}
+
+[ string ] $day = ( date ).dayofweek
+if ( $day ) {
+	echo "$day Macaroni Monday"
+}
+if ($day -eq 'Monday') {
+	echo "Macaroni Monday"
+} elseif ($day -eq 'Tuesday') {
+	echo "Taco Tuesday"
+} elseif ($day -eq 'Wednesday') {
+	echo "Waffle Wednesday"
+} elseif ($day -eq 'Thursday') {
+	echo "Talapia Thursday"
+} elseif ($day -eq 'Friday') {
+	echo "Falafel Friday"
+} elseif ($day -eq 'Saturday') {
+	echo "Sushi Saturday"
+} else {
+	echo "Schnitzel Sunday"
+}
+
+
